8cd9e0f13b638e0ea99f3136fe2670587e54b156
Gianfranco Cicciomessere <cicciomessereg@cardiff.ac.uk>
2024-03-20 18:46:21 +0000

    Noise makes a ripple effect

--- Scripts/Noise/PlayerNoise.gd
+++ Scripts/Noise/PlayerNoise.gd
@@ -1,3 +1,16 @@
 extends Area3D
 
 @export var TYPE := "Player"
+@export var noise_radius : float
+@export var one_shot : bool
+
+@onready var noise_particle = $NoiseParticle
+@onready var noise_collision = $NoiseCollision
+
+func _ready():
+	noise_collision.shape.radius = noise_radius + 0.001
+	noise_particle.one_shot = one_shot
+
+func _process(delta):
+	noise_particle.process_material.scale_min = noise_collision.shape.radius
+	noise_particle.process_material.scale_max = noise_collision.shape.radius
