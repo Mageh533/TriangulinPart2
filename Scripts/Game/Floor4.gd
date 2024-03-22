f09246a437d476603dc7784c8bf678da6ea943de
Gianfranco Cicciomessere <cicciomessereg@cardiff.ac.uk>
2024-03-22 12:38:42 +0000

    Spawn triangulin randomly in floor 4 when enetering a room

--- Scripts/Game/Floor4.gd
+++ Scripts/Game/Floor4.gd
@@ -3,7 +3,10 @@ extends Node3D
 # Nodes
 @onready var map_center_mark = $MapCenter
 @onready var estrellin_monster = $Estrellin
-@onready var ceiling = $BaseLayout/Floor5Ceiling
+@onready var triangulin_monster = $Triagulin
+@onready var ceiling = $BaseLayout/Ceiling
+
+var navigationMaps : Array[RID]
 
 var nextFloorLocked := true
 var pcDecryptable := false
@@ -12,6 +15,7 @@ var usbToPickUp : int = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	navigationMaps = NavigationServer3D.get_maps()
 	ceiling.show()
 
 func _on_key_pickup_key():
@@ -21,3 +25,11 @@ func _on_usb_picked_up():
 	usbPickedUp += 1
 	if usbPickedUp == usbToPickUp:
 		pcDecryptable = true
+
+func _on_spawn_trigger_body_entered(body):
+	var playerPos = body.global_position
+	playerPos.x += randf_range(-10, 10)
+	playerPos.z += randf_range(-10, 10)
+	var spawnTarget = NavigationServer3D.map_get_closest_point(navigationMaps[0], playerPos)
+	
+	triangulin_monster.appear(spawnTarget)
